e14646117fe3dd97747409dd1b1f5ad9c7031365
Simon Fishel <sfishel@splunk.com>
2016-09-14 16:49:59 -0700

    and removing the intentional visual regression

--- appserver/static/visualizations/timewrap/vrt/main.js
+++ appserver/static/visualizations/timewrap/vrt/main.js
@@ -5,7 +5,7 @@ import TimewrapChart from '../src/charting/TimewrapChart';
 import generateTimeSeries from '../test/generate-time-series';
 
 const timeSeries = generateTimeSeries('1981-08-18 23:15:00', 16, 15 * 60);
-const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ffffff'];
+const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff'];
 ReactDom.render(
     <TimewrapChart
         timeSeries={timeSeries}
