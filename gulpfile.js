d76e1b30c9f8b46cbf6a2839cdac75433c405ade
Simon Fishel <sfishel@splunk.com>
2016-09-25 08:40:24 -0700

    Adding gallery dashboard, finalizing the packaging task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,14 @@
 const fs = require('fs');
 const gulp = require('gulp');
 const replace = require('gulp-replace-task');
+const tar = require('gulp-tar');
+const gzip = require('gulp-gzip');
+const rename = require('gulp-rename');
 const gitRev = require('git-rev-sync');
 const exec = require('child_process').exec;
 const rimraf = require('rimraf');
+
+const packageInfo = JSON.parse(fs.readFileSync('./package.json'));
 
 gulp.task('clean', () => {
     rimraf.sync('build');
@@ -14,20 +19,42 @@ gulp.task('npm build', (cb) => {
     exec('npm run build', cb);
 });
 
+const configSrc = [
+    'default/**/*.*',
+    'metadata/**/*.*',
+    'README/**/*.*',
+    'static/**/*.*',
+    'lookups/**/*.*',
+];
 gulp.task('process config', ['clean'], () => {
-    gulp.src(['default/**/*.*', 'metadata/**/*.*', 'README/**/*.*', 'static/**/*.*'], { base: '.' })
+    gulp.src(configSrc, { base: '.' })
         .pipe(replace({
             patterns: [
-                { json: JSON.parse(fs.readFileSync('./package.json')) },
+                { json: packageInfo },
                 { json: { buildNumber: parseInt(gitRev.short(), 16) } },
             ],
         }))
         .pipe(gulp.dest('build'));
 });
 
+const vizFiles = [
+    'visualization.js',
+    'visualization.css',
+    'formatter.html',
+    'preview.png',
+];
+const staticSrc = [
+    `appserver/static/visualizations/timewrap/{${vizFiles.join(',')}}`,
+];
 gulp.task('process static', ['clean', 'npm build'], () => {
-    gulp.src(['appserver/**/*.*', '!**/*.map'], { base: '.' })
+    gulp.src(staticSrc, { base: '.' })
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['process config', 'process static']);
+gulp.task('default', ['process config', 'process static'], () => {
+    gulp.src('build/**/*.*')
+        .pipe(tar('build.tar'))
+        .pipe(gzip())
+        .pipe(rename(`${packageInfo.name}.spl`))
+        .pipe(gulp.dest('.'));
+});
